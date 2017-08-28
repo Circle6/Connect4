49d26c2a1aa27c630c94c33e569a125411b0da43
Donnie Thompson <donnie@carepoynt.com>
2017-08-28 13:34:13 -0700

    moved listening logic out of bin/www and added configs

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+import config from './server.config.js';
 import hbs from 'hbs';
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -52,4 +53,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(config.port, config.host, () => {
+  console.info('Express listening on port', config.port);
+});
